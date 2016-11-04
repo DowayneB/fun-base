977e1d8123cdec6b677a35aeba53317a31124546
DowayneB <dowayne.b@afrihost.com>
2016-11-04 21:47:08 +0200

    Update EchoCave.php

--- src/EchoCave.php
+++ src/EchoCave.php
@@ -6,11 +6,14 @@ class EchoCave
 {
     public function __construct($v = 'It worked! Try giving your own value! :)')
     {
-        $this->echoSomething($v);
+        $message = 'Thanks for leading me to this :)';
+        $link = 'http://stackoverflow.com/questions/4975411/when-do-i-use-php-eol-instead-of-n-and-vice-versa-ajax-jquery-client-problem';
+
+        $this->echoSomething($message,$link);
     }
 
-    public function echoSomething($v)
+    public function echoSomething($message,$link)
     {
-        echo $v . PHP_EOL;
+        echo $message . PHP_EOL . $link . PHP_EOL;
     }
 }
